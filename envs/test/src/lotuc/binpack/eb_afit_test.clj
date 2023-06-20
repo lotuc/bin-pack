dc5de416ca7953ed6a6c9008cf9eea6b5ac82fd9
lotuc <lotu.c@outlook.com>
2023-06-20 13:14:37 +0800

    chore(binpack/eb_afit): add some tests

--- envs/test/src/lotuc/binpack/eb_afit_test.clj
+++ envs/test/src/lotuc/binpack/eb_afit_test.clj
@@ -1,6 +1,7 @@
 (ns lotuc.binpack.eb-afit-test
   (:use clojure.test)
   (:require
+   [clojure.string :as s]
    [lotuc.binpack.eb-afit :as eb-afit]
    [lotuc.binpack.eb-afit-io :as eb-afit-io]
    [lotuc.binpack.eb-afit-io-test :as eb-afit-io-test]))
@@ -36,7 +37,33 @@
   ;; todo
   (testing "smallest-z gap has gap on both side."))
 
-(deftest find-best-test
+(deftest apply-ignore-gap-test
+  (testing "the gap to ignore is in middle"
+    (is (= [{:cumx 104, :cumz 84}]
+           (eb-afit/apply-ignore-gap
+            {:scrap-pad [{:cumx 30, :cumz 84}
+                         {:cumx 36, :cumz 77}
+                         {:cumx 104, :cumz 84}]
+             :smallest-z 1}))
+        "pre & pos's cumz is equal")
+    (is (= [{:cumx 36, :cumz 90}
+            {:cumx 104, :cumz 84}]
+           (eb-afit/apply-ignore-gap
+            {:scrap-pad [{:cumx 30, :cumz 90}
+                         {:cumx 36, :cumz 77}
+                         {:cumx 104, :cumz 84}]
+             :smallest-z 1}))
+        "pre cumz > pos cumz")
+    (is (= [{:cumx 36, :cumz 82}
+            {:cumx 104, :cumz 84}]
+           (eb-afit/apply-ignore-gap
+            {:scrap-pad [{:cumx 30, :cumz 82}
+                         {:cumx 36, :cumz 77}
+                         {:cumx 104, :cumz 84}]
+             :smallest-z 1}))
+        "pre cumz < pos cumz")))
+
+(deftest find-best-dpp-input-test
   (is (= [{:dims [10 10 10]
            :vol 1000
            :n 1
@@ -46,10 +73,43 @@
              eb-afit/exec-iterations
              :best-pack)))
 
-  (testing "can handle all test resources"
-    (doseq [n eb-afit-io-test/test-resources]
+  (testing "can handle dpp** resources"
+    (doseq [n (->> eb-afit-io-test/test-resources
+                   (filter #(s/includes? % "dpp")))]
       (println "testing" n)
-      (is (some? (-> (eb-afit-io/read-input-from-resource n)
-                     eb-afit/exec-iterations
-                     :best-pack))
-          (str "find-best-pack for " n)))))
+      (println
+       "!! testing" n "costs"
+       (with-out-str
+         (time
+          (is (some? (-> (eb-afit-io/read-input-from-resource n)
+                         eb-afit/exec-iterations
+                         :best-pack))
+              (str "find-best-pack for " n))))))))
+
+(deftest ^:find-best-large-input find-best-mpp-input-test
+  (testing "can handle mpp** resources"
+    (doseq [n (->> eb-afit-io-test/test-resources
+                   (filter #(s/includes? % "mpp")))]
+      (println "testing" n)
+      (println
+       "!! testing" n "costs"
+       (with-out-str
+         (time
+          (is (some? (-> (eb-afit-io/read-input-from-resource n)
+                         eb-afit/exec-iterations
+                         :best-pack))
+              (str "find-best-pack for " n))))))))
+
+(deftest ^:find-best-large-input find-best-rnd-input-test
+  (testing "can handle rnd** resources"
+    (doseq [n (->> eb-afit-io-test/test-resources
+                   (filter #(s/includes? % "rnd")))]
+      (println "testing" n)
+      (println
+       "!! testing" n "costs"
+       (with-out-str
+         (time
+          (is (some? (-> (eb-afit-io/read-input-from-resource n)
+                         eb-afit/exec-iterations
+                         :best-pack))
+              (str "find-best-pack for " n))))))))
