6c1b5104321825cdeeb7bf8cfc004961e895d5c7
lotuc <lotu.c@outlook.com>
2023-10-23 16:31:17 +0800

    chore: update

---
+++ js/binpack_worker.js
@@ -0,0 +1,4 @@
+importScripts("shared.js");
+(function(){
+'use strict';self.addEventListener("message",function(a){a=$APP.Oh(a.data,$APP.hd([$APP.Mh,!0]));var b=$APP.J(a,0,null);a=$APP.J(a,1,null);return $APP.gl(a,new $APP.t(null,2,[$APP.dl,function(c){postMessage($APP.Kl(new $APP.T(null,3,5,$APP.V,[b,"statistics",c],null)));return 0},$APP.el,function(c){return postMessage($APP.Kl(new $APP.T(null,3,5,$APP.V,[b,"res",c],null)))}],null))});
+}).call(this);
